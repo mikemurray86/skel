4d79d461e7c49219624bb138cf61de002d9f80ec
Mike Murray <mikemurray86@gmail.com>
2023-02-04 13:44:01 -0700

    Add documentation and unit test skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+//! Copies a template folder into the requested location.
+//!
+//! This module is used to read a template folder from a defined location and
+//! then copy it into the target folder. The template folder location can be specified
+//! in a configuration file or an environment variable. Inside the template folder will
+//! be folders containing each template. The template name will be the same name as the
+//! folder containing it.
+
 use figment::{
     providers::{Env, Format, Yaml},
     Figment,
@@ -14,6 +22,12 @@ struct Config {
     template_dir: String,
 }
 
+/// Returns a configuration file path to attempt to read data from.
+///
+/// If the $XDG_CONFIG_HOME environment variable is set then the file will
+/// be in `$XDG_CONFIG_HOME/skel.yaml` otherwise it will be in `$HOME/.config/skel.yaml`
+///
+/// if $HOME is not defined the function will panic
 fn get_config_path() -> OsString {
     let config_dir = match env::var_os("XDG_CONFIG_HOME") {
         Some(mut file) => {
@@ -29,6 +43,7 @@ fn get_config_path() -> OsString {
     env::var_os("SKEL_CONFIG").unwrap_or(config_dir)
 }
 
+/// Prints a basic help message to help show how to use the app
 fn print_help(name: &str) {
     println!(
         "\nUsage: {name} <template> <target>\n\
@@ -37,6 +52,8 @@ fn print_help(name: &str) {
     );
 }
 
+/// Main function that handles reading the config file for the template directory then
+/// picking the correct template and copying it's contents into the target location
 fn main() {
     let config: Config = Figment::new()
         .merge(Yaml::file(get_config_path()))
@@ -81,4 +98,24 @@ fn main() {
         fs::copy(src, dest).unwrap();
     }
     println!("Done!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn returns_config_dir() {
+        let home = env::var("HOME").unwrap();
+        let config = env::var("XDG_CONFIG_HOME").unwrap_or("".to_string());
+        env::set_var("HOME", "/home/test");
+        env::set_var("XDG_CONFIG_HOME", "/home/test/.config");
+        assert_eq!(get_config_path(), "/home/test/.config/skel.yaml");
+        env::set_var("HOME", home);
+        if config != "" {
+            env::set_var("XDG_CONFIG_HOME", config);
+        } else {
+            env::remove_var("XDG_CONFIG_HOME");
+        }
+    }
 }
